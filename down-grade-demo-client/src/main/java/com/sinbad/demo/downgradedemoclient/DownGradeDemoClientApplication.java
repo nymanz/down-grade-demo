86a9d53c3fb4b90fe157c55422b7360bde7dfa01
sunzhiwei1 <sunzhiwei1@xiaomi.com>
2020-07-05 11:08:47 +0800

    add client

---
+++ down-grade-demo-client/src/main/java/com/sinbad/demo/downgradedemoclient/DownGradeDemoClientApplication.java
@@ -0,0 +1,13 @@
+package com.sinbad.demo.downgradedemoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownGradeDemoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownGradeDemoClientApplication.class, args);
+	}
+
+}
---
+++ down-grade-demo-client/src/test/java/com/sinbad/demo/downgradedemoclient/DownGradeDemoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinbad.demo.downgradedemoclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownGradeDemoClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
