8d524f1ec2276ea3b729dbfc1d4aa13c8cbae877
sunzhiwei1 <sunzhiwei1@xiaomi.com>
2020-07-07 00:46:42 +0800

    add config

--- consumer-demo/src/main/java/com/sinbad/demo/webdemo/service/SentinelServiceImpl.java
+++ consumer-demo/src/main/java/com/sinbad/demo/webdemo/service/SentinelServiceImpl.java
@@ -1,6 +1,8 @@
 package com.sinbad.demo.webdemo.service;
 
 
+import java.util.Date;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.math.RandomUtils;
@@ -8,6 +10,9 @@ import org.springframework.stereotype.Service;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class SentinelServiceImpl {
 
@@ -28,9 +33,9 @@ public class SentinelServiceImpl {
 
 	@SentinelResource(value = "SentinelServiceImpl.sentinelTimeOut")
 	public String sentinelTimeOut() {
-
+		log.info("Do try, uuid={} date={}", UUID.randomUUID(), new Date().toString());
 		try {
-			TimeUnit.SECONDS.sleep(RandomUtils.nextInt(1000));
+			TimeUnit.SECONDS.sleep(RandomUtils.nextInt(10));
 		} catch (InterruptedException e) {
 			return e.getLocalizedMessage();
 		}
@@ -43,9 +48,9 @@ public class SentinelServiceImpl {
 	public String sentinelError() {
 
 		try {
-			return String.valueOf(1 / RandomUtils.nextInt(5));
+			return String.valueOf(1 / RandomUtils.nextInt(2));
 		} catch (Exception e) {
-			return e.getLocalizedMessage();
+			throw e;
 		}
 	}
 
